283fa5d4f802d4dd3048086efa16a5d4f2f3f6d7
pickle-radish <tkdansg0222@gmail.com>
2022-03-21 20:49:21 +0900

    3/21

---
+++ delivery/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from order.models import Shop, Menu, Order, Orderfood
+from order.serializers import ShopSerializer, MenuSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.utils import timezone
+
+@csrf_exempt
+def order_list(request):
+    if request.method == 'GET':
+        order_list = Order.objects.all()
+        return render(request, 'delivery/order_list.html', {'order_list': order_list})
+
+    elif request.method == 'POST':
+        order_item = Order.objects.get(pk=request.POST['order_id'])
+        order_item.deliver_finish = True
+
+        order_item.save()
+
+        return render(request, 'delivery/success.html')
+
+    else:
+        return HttpResponse(status=404)
--- online_order/urls.py
+++ online_order/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('order/', include('order.urls')),
+    path('boss/', include('boss.urls')),
+    path('delivery/', include('delivery.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
